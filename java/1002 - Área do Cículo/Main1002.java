e4c35f3a839d981c3548fd5635c513e7563b5e9f
renata21lima <renatafsdl@gmail.com>
2025-04-01 18:52:39 -0300

    feat: 1002

--- java/1002 - Área do Cículo/Main1002.java
+++ java/1002 - Área do Cículo/Main1002.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Main1002 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double raio = scan.nextDouble();
+
+        double A = 3.14159 * Math.pow(raio, 2);
+
+        System.out.printf("A=%.4f%n", A);
+
+        scan.close();
+    }
 }
